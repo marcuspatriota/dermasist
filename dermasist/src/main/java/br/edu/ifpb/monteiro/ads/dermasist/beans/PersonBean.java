2afc25632a3e619e6ba6de986a63e18902bcd1d0
cassio <cassio@Dell>
2014-07-17 20:42:07 -0300

    Folder structures changed, modifications on form and Search form created. Insertion of validations scripts and masks,.

---
+++ dermasist/src/main/java/br/edu/ifpb/monteiro/ads/dermasist/beans/PersonBean.java
@@ -0,0 +1,256 @@
+package br.edu.ifpb.monteiro.ads.dermasist.beans;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.view.ViewScoped;
+
+@ManagedBean(name = "personBean")
+@ViewScoped
+public class PersonBean implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+    
+    private List<String> states = new ArrayList<>();
+
+    private Long ID;
+    private String name;
+    private Date dateOfBirth;
+    private char sex;
+    private String cpf;
+    private String rg;
+    private String street;
+    private String number;
+    private String city;
+    private String neighborhood;
+    private String state;
+    private String mobile;
+    private String phone;
+
+    
+    public PersonBean(){
+        states.add("AC");
+        states.add("AL");
+        states.add("AM");
+        states.add("AP");
+        states.add("BA");
+        states.add("CE");
+        states.add("DF");
+        states.add("ES");
+        states.add("GO");
+        states.add("MA");
+        states.add("MG");
+        states.add("MS");
+        states.add("MT");
+        states.add("PA");
+        states.add("PB");
+        states.add("PE");
+        states.add("PI");
+        states.add("PR");
+        states.add("RJ");
+        states.add("RN");
+        states.add("RO");
+        states.add("RR");
+        states.add("RS");
+        states.add("SC");
+        states.add("SE");
+        states.add("SP");
+        states.add("TO");
+    }
+    
+    
+    public Date getDateToday(){
+        return new Date();
+    }
+    
+
+    /**
+     * @return the ID
+     */
+    public Long getID() {
+        return ID;
+    }
+
+    /**
+     * @param ID the ID to set
+     */
+    public void setID(Long ID) {
+        this.ID = ID;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    /**
+     * @return the sex
+     */
+    public char getSex() {
+        return sex;
+    }
+
+    /**
+     * @param sex the sex to set
+     */
+    public void setSex(char sex) {
+        this.sex = sex;
+    }
+
+    /**
+     * @return the cpf
+     */
+    public String getCpf() {
+        return cpf;
+    }
+
+    /**
+     * @param cpf the cpf to set
+     */
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    /**
+     * @return the rg
+     */
+    public String getRg() {
+        return rg;
+    }
+
+    /**
+     * @param rg the rg to set
+     */
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    /**
+     * @return the street
+     */
+    public String getStreet() {
+        return street;
+    }
+
+    /**
+     * @param street the street to set
+     */
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    /**
+     * @return the number
+     */
+    public String getNumber() {
+        return number;
+    }
+
+    /**
+     * @param number the number to set
+     */
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    /**
+     * @return the city
+     */
+    public String getCity() {
+        return city;
+    }
+
+    /**
+     * @param city the city to set
+     */
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    /**
+     * @return the neighborhood
+     */
+    public String getNeighborhood() {
+        return neighborhood;
+    }
+
+    /**
+     * @param neighborhood the neighborhood to set
+     */
+    public void setNeighborhood(String neighborhood) {
+        this.neighborhood = neighborhood;
+    }
+
+    /**
+     * @return the state
+     */
+    public String getState() {
+        return state;
+    }
+
+    /**
+     * @param state the state to set
+     */
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    /**
+     * @return the mobile
+     */
+    public String getMobile() {
+        return mobile;
+    }
+
+    /**
+     * @param mobile the mobile to set
+     */
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    /**
+     * @return the phone
+     */
+    public String getPhone() {
+        return phone;
+    }
+
+    /**
+     * @param phone the phone to set
+     */
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    /**
+     * @return the dateOfBirth
+     */
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    /**
+     * @param dateOfBirth the dateOfBirth to set
+     */
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public List<String> getStates() {
+        return states;
+    }
+    
+}
